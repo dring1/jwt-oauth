513e0bc6c608b54cafb2a1961739dd5c350c758a
Devon Ring <devon.ring@gmail.com>
2017-05-07 18:30:01 -0400

    fix merge

--- middleware/middlewares.go
+++ middleware/middlewares.go
@@ -14,10 +14,6 @@ const (
 func New(svcs *services.Services) (MiddlewareMap, error) {
 	m := make(MiddlewareMap)
 	m[ValidateMiddleware] = NewTokenValidationMiddleware(svcs.TokenService)
-<<<<<<< HEAD
-	m[LoggingMiddleware] = NewApacheLoggingHandler()
-=======
 	//m[LoggingMiddleware] = NewApacheLoggingHandler(svcs.)
->>>>>>> 4fab027b6560c49e20448e9d6d0f6f33d55d5287
 	return m, nil
 }
